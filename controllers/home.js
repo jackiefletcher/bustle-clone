e374236ef895758e5f9915c8ef5531be618814a4
Debbie Cravey and Jackie Fletcher <cinemaestro@yahoo.com>
2015-03-02 15:20:46 -0800

    attempt at Ember carousel

--- controllers/home.js
+++ controllers/home.js
@@ -1,4 +1,5 @@
 HustledBustle.HomeController = Ember.Controller.extend({
+
   isPictureShowing: false,
   actions: {
     showPicture: function() {
@@ -9,3 +10,35 @@ HustledBustle.HomeController = Ember.Controller.extend({
     // }
   }
 });
+
+
+
+//Ember version (not yet functioning)
+// HustledBustle.viewArray = Ember.A(["aTemplate", "bTemplate", "cTemplate"]);
+
+// currentIndex: 0,
+//   selectedView: function() {
+//     currentIndex = this.get('currentIndex');
+//     template = HustledBustle.viewArray.get(currentIndex);
+//       return Ember.View.create({
+//           templateName: template,
+//           classNames: ["carousel-element"],
+//           didInsertElement: function() {
+//               $(this.$()[0]).animate({
+//                   opacity: 1,
+//                   marginLeft: '+=100'
+//               }, 500);
+//           }
+//       });
+//   }.property('currentIndex'),
+//   previous: function() {
+//       if (this.get('currentIndex') !== 0) {
+//           currentIndex = this.get('currentIndex');
+//           this.set('currentIndex', currentIndex - 1);
+//       }
+//   },
+//   next: function() {
+//       if (this.get('currentIndex') !== (HustledBustle.viewArray.length - 1)) {
+//           currentIndex = this.get('currentIndex');
+//           this.set('currentIndex', currentIndex + 1);
+//       }
